f3a73093df8b4cd56f12eab7ba7178b1b85287b0
david9833 <dagalisteo@gmail.com>
2022-12-06 11:36:17 +0000

    My first commit

--- src/app.py
+++ src/app.py
@@ -15,6 +15,7 @@ CORS(app)
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -26,17 +27,45 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
-
+def get_members():
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
     response_body = {
-        "hello": "world",
         "family": members
     }
-
-
     return jsonify(response_body), 200
+
+
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_single_member(member_id):
+    member = jackson_family.get_member(member_id)
+    response_body = {
+    "family": member
+    }
+    return jsonify(response_body), 200
+
+
+@app.route('/member/<int:member_id>', methods=['POST'])
+def post_single_member(member_id):
+    body = request.get_json()
+    member = {
+    "id":member_id,
+    "first_name": body["first_name"],
+    "age":body["age"],
+    "lucky_numbers": body["lucky_numbers"]
+    }
+    new_member = jackson_family.add_member(member)
+    return jsonify({"message":"New member added "}),200
+
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_single_member(member_id):
+    member = jackson_family.delete_member(member_id)
+    response_body = {
+    "Message":"Member deleted"
+    }
+    return jsonify(response_body), 200
+    
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
